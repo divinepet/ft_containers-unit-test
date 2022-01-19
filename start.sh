93c14e334d395714af704d59f01589ea76361207
bendordie <79161069+bendordie@users.noreply.github.com>
2022-01-19 15:02:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,23 +11,23 @@ then
 fast_flag="-DFAST"
 fi
 
-if [[ (($1 = -m || $1 = --map) && $2 = -fast || $2 = '') || (($2 = -m || $2 = --map) && $1 = -fast) ]]
+if [[ (($1 = -m || $1 = --map) && ($2 = -fast || $2 = '')) || (($2 = -m || $2 = --map) && $1 = -fast) ]]
 then
   cat credits/header
   clang++ $fast_flag run_map.cpp && ./a.out
-elif [[ (($1 = -v || $1 = --vector) && $2 = -fast || $2 = '') || (($2 = -v || $2 = --vector) && $1 = -fast) ]]
+elif [[ (($1 = -v || $1 = --vector) && ($2 = -fast || $2 = '')) || (($2 = -v || $2 = --vector) && $1 = -fast) ]]
 then
   cat credits/header
   clang++ $fast_flag run_vector.cpp && ./a.out
-elif [[ (($1 = -s || $1 = --set) && $2 = -fast || $2 = '') || (($2 = -s || $2 = --set) && $1 = -fast) ]]
+elif [[ (($1 = -s || $1 = --set) && ($2 = -fast || $2 = '')) || (($2 = -s || $2 = --set) && $1 = -fast) ]]
 then
   cat credits/header
   clang++ $fast_flag run_set.cpp && ./a.out
-elif [[ (($1 = -u || $1 = --utils) && $2 = -fast || $2 = '') || (($2 = -u || $2 = --utils) && $1 = -fast) ]]
+elif [[ (($1 = -u || $1 = --utils) && ($2 = -fast || $2 = '')) || (($2 = -u || $2 = --utils) && $1 = -fast) ]]
 then
   cat credits/header
   clang++ $fast_flag run_utilities.cpp && ./a.out
-elif [[ (($1 = -st || $1 = --stack) && $2 = -fast || $2 = '') || (($2 = -st || $2 = --stack) && $1 = -fast) ]]
+elif [[ (($1 = -st || $1 = --stack) && ($2 = -fast || $2 = '')) || (($2 = -st || $2 = --stack) && $1 = -fast) ]]
 then
   cat credits/header
   clang++ $fast_flag run_stack.cpp && ./a.out
