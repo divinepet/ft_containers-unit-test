db67101a81060e98f41788c96a94c5b0164008ee
Carmel Shells <cshells@er-c3.kzn.21-school.ru>
2022-02-20 16:40:16 +0300

    Fixed vector insert(fill) test, before it used insert(range)

--- sources/vector_tests/insert(fill).cpp
+++ sources/vector_tests/insert(fill).cpp
@@ -4,13 +4,11 @@
 template <typename T>
 std::vector<int> insert_test_2(std::vector<T> vector) {
     std::vector<int> v;
-    std::vector<int> tmp;
-    tmp.assign(2600 * _ratio, 1);
-    vector.assign(2200 * _ratio, 1);
+    vector.assign(1000, 1);
     g_start1 = timer();
-    vector.insert(vector.end() - 1400 * _ratio, tmp.begin(), tmp.end());
+    vector.insert(vector.end() - 50, 4200 * _ratio , 2);
     g_end1 = timer();
-    v.push_back(vector[3]);
+    v.push_back(vector[2121]);
     v.push_back(vector.size());
     v.push_back(vector.capacity());
     return v;
@@ -19,13 +17,11 @@ std::vector<int> insert_test_2(std::vector<T> vector) {
 template <typename T>
 std::vector<int> insert_test_2(_vector<T> vector) {
     std::vector<int> v;
-    _vector<int> tmp;
-    tmp.assign(2600 * _ratio, 1);
-    vector.assign(2200 * _ratio, 1);
+    vector.assign(1000, 1);
     g_start2 = timer();
-    vector.insert(vector.end() - 1400 * _ratio, tmp.begin(), tmp.end());
+    vector.insert(vector.end() - 50, 4200 * _ratio , 2);
     g_end2 = timer();
-    v.push_back(vector[3]);
+    v.push_back(vector[2121]);
     v.push_back(vector.size());
     v.push_back(vector.capacity());
     return v;
