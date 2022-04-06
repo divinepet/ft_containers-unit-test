ecdbeced6bcd6eb3be4954432432f4ba73c18dce
Carmel Shells <cshells@ev-d4.kzn.21-school.ru>
2022-04-06 22:56:43 +0300

    Fixed insert(value): move exception test to insert(fill)

--- sources/vector_tests/insert(range).cpp
+++ sources/vector_tests/insert(range).cpp
@@ -1,6 +1,35 @@
 #include "../system/system_methods.ipp"
 #include "__service.ipp"
 
+class B {
+public:
+    char *l;
+    int i;
+    B():l(nullptr), i(1) {};
+    B(const int &ex) {
+        this->i = ex;
+        this->l = new char('a');
+    };
+    virtual ~B() {
+        delete this->l;
+        this->l = nullptr;
+    };
+};
+
+class A : public B {
+public:
+    A():B(){};
+    A(const B* ex){
+        this->l = new char(*(ex->l));
+        this->i = ex->i;
+        if (ex->i == -1) throw "n";
+    }
+    ~A() {
+        delete this->l;
+        this->l = nullptr;
+    };
+};
+
 template <typename T>
 std::vector<int> insert_test_3(std::vector<T> vector) {
     std::vector<int> v;
@@ -13,6 +42,22 @@ std::vector<int> insert_test_3(std::vector<T> vector) {
     v.push_back(vector[3]);
     v.push_back(vector.size());
     v.push_back(vector.capacity());
+
+    std::unique_ptr<B> k2(new B(3));
+    std::unique_ptr<B> k3(new B(4));
+    std::unique_ptr<B> k4(new B(-1));
+    std::vector<A> vv;
+    std::vector<B*> v1;
+
+    v1.push_back(&(*k2));
+    v1.push_back(&(*k3));
+    v1.push_back(&(*k4));
+    try { vv.insert(vv.begin(), v1.begin(), v1.end()); }
+    catch (...) {
+        v.push_back(vv.size());
+        v.push_back(vv.capacity());
+    }
+
     return v;
 }
 
@@ -28,6 +73,22 @@ std::vector<int> insert_test_3(_vector<T> vector) {
     v.push_back(vector[3]);
     v.push_back(vector.size());
     v.push_back(vector.capacity());
+
+    std::unique_ptr<B> k2(new B(3));
+    std::unique_ptr<B> k3(new B(4));
+    std::unique_ptr<B> k4(new B(-1));
+    _vector<A> vv;
+    _vector<B*> v1;
+
+    v1.push_back(&(*k2));
+    v1.push_back(&(*k3));
+    v1.push_back(&(*k4));
+    try { vv.insert(vv.begin(), v1.begin(), v1.end()); }
+    catch (...) {
+        v.push_back(vv.size());
+        v.push_back(vv.capacity());
+    }
+
     return v;
 }
 
